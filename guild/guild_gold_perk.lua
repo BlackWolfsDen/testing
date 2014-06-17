da51d7739b6efad9a4bc4f0c98f45d2aa7aa36ec
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-06-16 19:50:28 -0600

    Update guild_gold_perk.lua

--- guild/guild_gold_perk.lua
+++ guild/guild_gold_perk.lua
@@ -1,6 +1,7 @@
+ -- this example will deposit 10% of looted gold,silver,copper into players guild bank
 local function OnLootMoney(eventid, player, amount)
 	if (player:IsInGuild()) then
-		player:GetGuild():DepositBankMoney(player, math.floor(amount*0.1)) -- this example will deposit 10% of looted gold,silver,copper
+		player:GetGuild():DepositBankMoney(player, math.floor(amount*0.1))
 	end
 end
 
