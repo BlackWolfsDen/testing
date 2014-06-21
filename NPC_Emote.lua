f1edd74abfbb9634cc0e49215ed964777815bba2
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-06-20 21:50:09 -0600

    Update NPC_Emote.lua
    hehe

--- NPC_Emote.lua
+++ NPC_Emote.lua
@@ -2,7 +2,8 @@
 -- if the emote matches a pre-determined emote value then a pre-determined return emote
 -- will happen. orrr... the creature may just attack you.... its a work in progress
 
-local Emotemax = 10
+local Emotemax = 20
+local percent = 0.25
 				
 local function NPC_EMOTE(event, creature, player, emoteid)
 
@@ -10,11 +11,12 @@ local Reactor = math.random(1, Emotemax)
 local Reaction = math.random(1, Emotemax+1)
 
 	if(emoteid==Reactor)then
-		
-		if(emoteid < Emotemax+1)then
+		if(Reaction < (Emotemax-(Emotemax*percent)))then
 			creature:Emote(Reaction)
+			print("emote")
 		else
 			creature:AttackStart(player)
+			print("attack")
 		end
 	end
 end
