ea1d140344083881fb682e369d486c953b64f923
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-06-25 05:43:52 -0600

    Update Salja_Premium_System.lua
    updated for dynamics and tables n ladders n chairs oh my.

--- Salja_Premium_System.lua
+++ Salja_Premium_System.lua
@@ -1,18 +1,29 @@
 -- NOTE: need to insert character_premium.sql in your characters database
+ACCT = {}
+print ("Premium Table Allocated.")
+-- Trinity using Eluna
+
+function Player_Premium_Table(event, player)
+	local Q = WorldDBQuery("SELECT username, premium FROM auth.account WHERE `id` = '"..player:GetAccountId().."';"); -- this would need to be changed for your Premium value location.
+	PREM[player:GetAccountId()] = {
+		Name = Q:GetString(0),
+		Premium = Q:GetUInt32(1)
+									};
+	print(PREM[player:GetAccountId()].Name.." :Premium table loaded.")
+print(PREM[player:GetAccountId()].Premium)
+end
 
 local function PremiumOnLogin(event, player)  -- Send a welcome massage to player and tell him is premium or not
-    local result = CharDBQuery("SELECT AccountId FROM premium WHERE active=1 and AccountId = "..player:GetAccountId())
-    if (result) then
-        player:SendBroadcastMessage("|CFFE55BB0[Premium]|r|CFFFE8A0E Welcome "..player:GetName().." you are Premium! |r")
+   if(PREM[player:GetAccountId()].Premium==1)then
+		player:SendBroadcastMessage("|CFFE55BB0[Premium]|r|CFFFE8A0E Welcome "..player:GetName().." you are Premium! |r")
     else
-        player:SendBroadcastMessage("|CFFE55BB0[Premium]|r|CFFFE8A0E Welcome "..player:GetName().." you are NOT Premium! |r")
+		player:SendBroadcastMessage("|CFFE55BB0[Premium]|r|CFFFE8A0E Welcome "..player:GetName().." you are NOT Premium! |r")
     end
 end
 
 local function PremiumOnChat(event, player, msg, _, lang)
-    local result = CharDBQuery("SELECT AccountId FROM premium WHERE active=1 and AccountId = "..player:GetAccountId())
-    if (msg == "#premium") then  -- Use #premium for sending the gossip menu
-        if (result) then
+	if (msg == "#premium") then  -- Use #premium for sending the gossip menu
+		if(PREM[player:GetAccountId()].Premium==1)then
             OnPremiumHello(event, player)
         else
             player:SendBroadcastMessage("|CFFE55BB0[Premium]|r|CFFFE8A0E Sorry "..player:GetName().." you are NOT Premium! |r")
@@ -22,25 +33,29 @@ end
 
 function OnPremiumHello(event, player)
     player:GossipClearMenu()
-    player:GossipMenuAddItem(0, "Show Bank", 0, 3)
-    player:GossipMenuAddItem(0, "Show AuctionsHouse", 0, 4)
-    player:GossipMenuAddItem(0, "Armor", 0, 5)
-    player:GossipMenuAddItem(0, "Weapons", 0, 6)
-    player:GossipMenuAddItem(0, "Misc Premium Items", 0, 7)
+    player:GossipMenuAddItem(0, "Show Bank", 0, 2)
+    player:GossipMenuAddItem(0, "Show AuctionsHouse", 0, 3)
+    player:GossipMenuAddItem(0, "Armor", 0, 4)
+    player:GossipMenuAddItem(0, "Weapons", 0, 5)
+    player:GossipMenuAddItem(0, "Misc Premium Items", 0, 6)
     player:GossipMenuAddItem(0, "Nevermind..", 0, 1)
     player:GossipSendMenu(1, player, 100)
 end
 
 function OnPremiumSelect(event, player, _, sender, intid, code)
-  if (intid == 1) then                     -- Close the Gossip
+	
+	if(intid==1) then                     -- Close the Gossip
         player:GossipComplete()
-  elseif (intid == 2) then                 -- Go back to main menu
-        OnPremiumHello(event, player)
-  elseif (intid == 3) then                 -- Send Bank Window
+ 	elseif(intid==2) then                 -- Send Bank Window
         player:SendShowBank(player)
-  elseif (intid == 4) then                 -- Send Auctions Window
+	elseif(intid==3) then                 -- Send Auctions Window
         player:SendAuctionMenu(player)
-  end
+	elseif(intid==4)then
+	elseif(intid==5)then
+	elseif(intid==6)then
+	elseif(intid > 6) then                 -- Go back to main menu
+        OnPremiumHello(event, player)
+	end
     -- Room for more premium things
 end
 
