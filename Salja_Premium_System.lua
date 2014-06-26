156a8e9379b93704eb459d19807dab1bf8cba2dc
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-06-25 20:09:04 -0600

    Update Salja_Premium_System.lua

--- Salja_Premium_System.lua
+++ Salja_Premium_System.lua
@@ -1,24 +1,31 @@
--- NOTE: need to insert character_premium.sql in your characters database
-ACCT = {}
-print ("Premium Table Allocated.")
--- Trinity using Eluna
+-- Premium System by Salja of emudevs.com
+-- updated by slp13at420 of emudevs.com
+-- NOTE: need to insert character_premium.sql in your auth.account database.table
+-- just add 1 column to your auth.account table:
+-- name `premium` : Datatype = TINYINT : Length/Set = 1 : Unsigned = checked : Default = 0
 
-function Player_Premium_Table(event, player)
-	local Q = WorldDBQuery("SELECT username, premium FROM auth.account WHERE `id` = '"..player:GetAccountId().."';"); -- this would need to be changed for your Premium value location.
-	PREM[player:GetAccountId()] = {
-		Name = Q:GetString(0),
-		Premium = Q:GetUInt32(1)
-					};
-end
+-- NOT YET TESTED . just theoretical . I MUST test this FIRST .. give it a good work over  .. make it smooooooootttthhhhhhhhhh. yea ... ocd...
+-- i will remove lines 6 4x when its done
+-- for TrintyCore2 3.3.5 Eluna
+
+ACCT = {}
+print ("Premium System Table: initialized and allocated.")
 
 local function PremiumOnLogin(event, player)  -- Send a welcome massage to player and tell him is premium or not
+
+local Q = WorldDBQuery("SELECT username, premium FROM auth.account WHERE `id` = '"..player:GetAccountId().."';"); -- this would need to be changed for your Premium value location.
+
+PREM[player:GetAccountId()] = {
+	Name = Q:GetString(0),
+	Premium = Q:GetUInt32(1)
+				};
+			
 	if(PREM[player:GetAccountId()].Premium==1)then
 		player:SendBroadcastMessage("|CFFE55BB0[Premium]|r|CFFFE8A0E Welcome "..player:GetName().." you are Premium! |r")
 	else
 		player:SendBroadcastMessage("|CFFE55BB0[Premium]|r|CFFFE8A0E Welcome "..player:GetName().." you are NOT Premium! |r")
 		player:SendBroadcastMessage("|CFFE55BB0[Premium]|r|CFFFE8A0E You can donate to earn the Premium Rank.|r")
     	end
-print(PREM[player:GetAccountId()].Premium)
 print(PREM[player:GetAccountId()].Name.." :Premium table loaded.")
 end
 
@@ -45,17 +52,18 @@ end
 
 function OnPremiumSelect(event, player, _, sender, intid, code)
 	
-	if(intid==1) then                     -- Close the Gossip
+	if(intid==1) then               -- Close the Gossip
         player:GossipComplete()
- 	elseif(intid==2) then                 -- Send Bank Window
+ 	elseif(intid==2) then           -- Send Bank Window
         player:SendShowBank(player)
-	elseif(intid==3) then                 -- Send Auctions Window
+	elseif(intid==3) then           -- Send Auctions Window
         player:SendAuctionMenu(player)
-	elseif(intid==4)then
-	elseif(intid==5)then
-	elseif(intid==6)then
-	elseif(intid > 6) then                 -- Go back to main menu
-        OnPremiumHello(event, player)
+	elseif(intid==4)then		-- do something
+	elseif(intid==5)then		-- do something
+	elseif(intid==6)then		-- do something
+	elseif(intid > 6) then          -- Go back to main menu
+	player:GossipComplete()
+	OnPremiumHello(event, player)
 	end
     -- Room for more premium things
 end
