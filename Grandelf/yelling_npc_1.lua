94267f9bdc20152f15dac107b278900812145d03
Grumbo <blackwolfsden@hotmail.com>
2014-11-03 18:15:33 -0700

    Update yelling_npc_1.lua

--- Grandelf/yelling_npc_1.lua
+++ Grandelf/yelling_npc_1.lua
@@ -3,27 +3,51 @@ local range = 40; -- Range that will trigger / stop the npc from announcing.
 local delay = 1*10*1000; -- Delay between the announcements.
 local subDelay = 1*2*1000; -- Time between linked announcements.
 
--- {Statement, stated, linked, emote, spellid,{spawn type, spawn id},},
--- statement :: in quotes "blah blah" 
--- stated :: say = 0 // yell = 1 
--- linked :: table key id if your using multiple statements  for one announcement i.e.(yell THEN say)
--- emote :: talk = 1 // yell = 5// Question = 6 // Dance = 10 // Rude = 14 // shout = 22 // 
--- spellid :: spell id
--- http://collab.kpsn.org/display/tc/Emote
--- spawn type :: 0 none, 1 npc // 2 gob
--- spawn id :: id of what to spawn
-
 local annTable = { ["Says"] = {
+	-- [index] = {msg, msgType, linkId, emoteId, spellId},
 	[1] = {"Yo, I heard you liked boars!", 1, 0, 6, 58837}, -- Announce with spell cast: 58837.
 	[2] = {"What's up dawg?", 0, 100, 1, 0}, -- Announcement say, linked to 100.
 	[100] = {"Yo, you good dawg", 0, 101, 1, 0}, -- Announcement say, linked to 101 and linked from 2.
 	[101] = {"Hey, I am talking to you!", 1, 0, 1, 0}, -- Announcement yell, linked from 100.
 }};
 
+local getCreature = {
+	__newindex = function(tbl, key, value)	
+		local guid = key:GetGUID();
+		if value == false then
+			tbl.__cache[guid] = nil; return;
+		end
+		
+		local map = GetMapById(key:GetMapId(), 0);
+		-- Set the table, so we know the announcer
+		-- started announcing.
+		tbl.__cache[guid] = map;
+	end,	
+	__call = function(tbl, ...)
+		local guid = select(1, ...);
+		-- Does the table exist already?
+		if tbl.__cache[guid] == nil then
+			return nil;
+		end
+		local map = tbl.__cache[guid];
+		-- Return the object because that's the value
+		-- we want.
+		return map:GetWorldObject(guid);
+	end,
+	__cache = {},
+}
+setmetatable(getCreature, getCreature);
+		
 -- Amount values we need in a single table
 local reqData = 5; -- {msg, msgType, linkId, emoteId, spellId} = 5.
 
-local function announce(id, pUnit)
+local function announce(id, guid)
+	local pUnit = getCreature(guid);
+	-- If the unit isn't active, we don't want to run this function.
+	if type(pUnit) ~= "userdata" then
+		return;
+	end
+	
 	local sayTable = annTable["Says"][id];
 	-- Are we missing data? We don't want any nil values ...
 	if #sayTable ~= reqData then
@@ -42,21 +66,15 @@ local function announce(id, pUnit)
 			return;
 		end
 		-- Recursive call to announce, we want the same result right?
-		CreateLuaEvent(function() announce(linkId, pUnit); end, subDelay, 1);
+		CreateLuaEvent(function() announce(linkId, pUnit:GetGUID()); end, subDelay, 1);
 	end
 	if spellId ~= 0 then pUnit:CastSpell(pUnit, spellId); end
 end
-	
-local function timedSay(_, _, _, pUnit)
-	announce(math.random(#annTable["Says"]), pUnit);
-end
 
-local function stopAnnounce(pUnit)
-	if #pUnit:GetPlayersInRange(range) == 0 then
-		annTable[pUnit:GetGUIDLow()] = false;
-		return pUnit:RemoveEvents();
-	end
-	CreateLuaEvent(function() return stopAnnounce(pUnit); end, delay, 1);
+-- No need to check for active unit here, because this won't
+-- be called if the unit isn't active.	
+local function timedSay(_, _, _, pUnit)
+	announce(math.random(#annTable["Says"]), pUnit:GetGUID());
 end
 
 local function onMotion(_, pUnit, plr)
@@ -65,18 +83,33 @@ local function onMotion(_, pUnit, plr)
 	if #pUnit:GetPlayersInRange(range) == 0 then
 		return;
 	end
-	
-	local lGuid = pUnit:GetGUIDLow();
-	local unitTable = annTable[lGuid];
 	-- Are we announcing?
-	if unitTable == nil or unitTable == false then
-		annTable[lGuid] = true; -- We are now ...
+	if type(getCreature(pUnit:GetGUID())) ~= "userdata" then
+		getCreature[pUnit] = true; -- We are now ...
 		pUnit:RegisterEvent(timedSay, delay, 0);
-		-- Registering the check to see if we should continue 
-		-- to announce or not.
-		CreateLuaEvent(function() return stopAnnounce(pUnit); end, delay-1, 1);
 	end	
 end	
+
+local function cleanTable()
+	-- Are there any creatures in the table?
+	local cacheTable = getCreature.__cache
+	if next(cacheTable) == nil then
+		return;
+	end
+	-- Lets check for creatures that aren't active
+	-- and clean the table if that's the case.
+	for key, v in pairs(cacheTable) do
+		-- Is unit active?
+		local pUnit = getCreature(key);
+		if type(pUnit) ~= "userdata" then
+			getCreature[pUnit] = false;
+		elseif #pUnit:GetPlayersInRange(range) == 0 then
+			getCreature[pUnit] = false;
+		end
+	end
+end
+CreateLuaEvent(cleanTable, 5000, 0);
+	
 		
 for index=1, #npcList do
 	RegisterCreatureEvent(npcList[index], 27, onMotion);
